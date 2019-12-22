b753b86431ad215d873776e2fb9089ff0bf94ccd
Ricky dildy <rdildy92@gmail.com>
2019-12-22 23:46:47 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -17,8 +17,7 @@ end
     short_words = ["puff", "go", "two"]
     assorted_words = ["two", "go", "industrious", "bop"]
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls()
 end
 
 def find_the_cheese# code an argument here
