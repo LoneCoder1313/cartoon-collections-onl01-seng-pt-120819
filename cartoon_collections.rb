ef845b8258970018a70c429b9ba817c27d0ddbf6
Ricky dildy <rdildy92@gmail.com>
2019-12-22 23:40:23 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -4,8 +4,9 @@ def roll_call_dwarves(array)
   end 
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+ roll_call_dwarves
+
+def summon_captain_planet()
 end
 
 def long_planeteer_calls# code an argument here
