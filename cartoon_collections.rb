ced2556905cb0179ebfc63ea232abe776be6d16d
Ricky dildy <rdildy92@gmail.com>
2019-12-22 23:49:53 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -23,7 +23,6 @@ end
     long_planeteer_calls(short_words)
      long_planeteer_calls(assorted_words)
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
