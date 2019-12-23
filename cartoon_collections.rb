031f69c1801278193a3da9c93acc3d09eb85c69e
Ricky dildy <rdildy92@gmail.com>
2019-12-23 00:19:10 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,30 +11,21 @@ def summon_captain_planet(array)
     planeteer_calls << "#{calls.upcase}!"
 end
     planeteer_calls
+end 
+
+def long_planeteer_calls(short_words)
+   short_words.any? do |word|
+     word.length > 4
+   end 
+end 
 
 
-def long_planeteer_calls(short_words) 
-  i = 0 
-  if  short_words.any? {|i| i.length > 4}
-    return true
-  else 
-    return false
-  i = i + 1
-  end 
-end
-
-long_planeteer_calls(short_words, assorted_words)
-    
-
-
-cheese_types = ["cheddar", "gouda", "camembert"]
-snacks = ["crackers", "gouda", "thyme"]
-
+     
+     cheese_types = ["cheddar", "gouda", "camembert"]
+     snacks = ["crackers", "gouda", "thyme"]
 
 def find_the_cheese(array)
-  cheese_types = ["cheddar", "gouda", "camembert"]
-    array.find do |type|
-    cheese_types.include?(type)
+  array.find do |cheese|
   end 
 end
 
