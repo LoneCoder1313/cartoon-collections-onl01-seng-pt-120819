c3d5155c3701d9762367e5a40fcfa877f2929b6e
Ricky dildy <rdildy92@gmail.com>
2019-12-23 00:03:28 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,36 +1,3 @@
-# def roll_call_dwarves(array)
-#   array.each_with_index do |name, index|
-#     puts "#{index+1}. #{name}"
-#   end 
-# end
-
-# roll_call_dwarves
-
-# def summon_captain_planet(planeteer_calls)
-#   planeteer_calls.collect do |word|
-#       word.capitalize + "!"
-#     end 
-# end
-
-#     summon_captain_planet(planeteer_calls)
-   
-# def long_planeteer_calls(short_words)
-#     short_words.any? do |word|
-#       word.length > 4
-#     end 
-# end
-
-#     long_planeteer_calls(short_words)
-#     long_planeteer_calls(assorted_words)
-
-# def find_the_cheese(cheese)
-#   cheese_types = ["cheddar", "gouda", "camembert"]
-#   cheese.find do |type|
-#     cheese_types.include?(type)
-#   end 
-# end
-#     find_the_cheese(snacks)
-
  def roll_call_dwarves(array)
   i = 0 
   while i < array.length 
@@ -39,7 +6,7 @@
   end 
 end
 
-roll_call_dwarves(dwarves)
+roll_call_dwarves(array)
 
 
 
